c0fff1c8f85c9abf760c5c0c8f2d24645ca253ec
MatheuzH <matheusherzog77@gmail.com>
2022-11-22 22:28:15 -0300

    arquivos

---
+++ Cards/script.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+    exebir_jogadores()
+
+}
+
+function exebir_jogadores(){
+    let card = document.querySelector(".CardsInternos")
+    let jogador = document.cookie.split('$')
+
+    console.log(jogador)
+
+    card.innerHTML += `
+        <img src='${jogador[0]}'>
+        <h1 class="letras_cards">${jogador[1]}</h1>
+        <h1 class="letras_cards">${jogador[5]}</h1>
+        <h1 class="letras_cards">${jogador[6]}</h1>
+
+        <button class="botao" onclick='voltar()'>voltar</button>
+    `;
+}
+
+function voltar(){
+    document.cookie = ''
+
+    window.location.href = '../../';
+
+}
+
---
+++ script.js
@@ -0,0 +1,56 @@
+let conteiner = document.querySelector(".conteiner")
+
+function Mostrar_cards(){
+    for (let i = 0; i <= lista_jogadores.length; i++){
+
+        conteiner.innerHTML += `
+            <div class="cards">
+                <img src="${lista_jogadores[i]["imagem"]}">
+                <h2>${lista_jogadores[i]["nome"]}</h2>
+                <p>${lista_jogadores[i]["posicao"]}</p>
+                <button class="button" onclick='Cookie(${JSON.stringify(lista_jogadores[i])})'>
+                    Sobre o jogador
+                </button>
+            </div>
+        `;
+    }
+
+}
+
+function pesquisar(){
+    const pesquisa = document.getElementById("espaco_pesquisa").value.toLowerCase()
+    const card_jogador = document.getElementsByClassName("cards")
+
+    for (let i = 0; i < card_jogador.length; i++){
+        let nome_jogador = card_jogador[i].querySelector(".cards h2")
+    
+
+    if(nome_jogador.innerText.toLowerCase().indexOf(pesquisa) > -1){
+        card_jogador[i].style.display = ""
+    } else{
+        card_jogador[i].style.display = "none"
+    }
+
+    }
+
+}
+
+
+
+function Cookie(jogador){
+    document.cookie = jogador["imagem"] + "$";
+    document.cookie += jogador["nome_completo"] + "$";
+    document.cookie += jogador["posicao"] + "$";
+    document.cookie += jogador["nome"] + "$";
+    document.cookie += jogador["descricao"] + "$";
+    document.cookie += jogador["nascimento"] + "$";
+    document.cookie += jogador["altura_peso"] + "$";
+
+    window.location.href = './Cards/cards.html'
+
+    document.cookie = ""
+}
+
+window.onload = () => {
+    Mostrar_cards()
+}
